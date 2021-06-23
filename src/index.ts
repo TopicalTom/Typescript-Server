1995da680335b32843f0faafc29f5b69310762d4
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-23 16:35:39 -0400

    Added login form and redirect logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Lets go!');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['lasdad'] }));
+app.use(router);
+
+app.listen(3000, () => {
+    console.log('Listenting on port 3000');
+});
