5d5bf3756add54743314fa3ea6a5d072aaa5fb10
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-23 16:41:43 -0400

    Added logged in and logged out routes

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -19,3 +19,16 @@ router.post('/login', function (req, res) {
     }
     ;
 });
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n            <div>\n                <div>You are logged in</div>\n                <a href=\"/logout\">Logout</a>\n            </div>\n        ");
+    }
+    else {
+        res.send("\n            <div>\n                <div>You are not logged in</div>\n                <a href=\"/login\">Login</a>\n            </div>\n        ");
+    }
+    ;
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    res.redirect('/');
+});
